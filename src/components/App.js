df611ff25348fecf7aad8f64b4df70aacbbe71a6
Dion Theroulde <dionjr105@gmail.com>
2021-07-14 09:29:21 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+  
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(data =>setQuestions(data))
+  },[]) 
+
+  const deleteQuestion = (id) => {
+   const updatedQuestion = questions.filter(question => question.id !== id) 
+    setQuestions(updatedQuestion)
+  }
+
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm questions={questions} setQuestions={setQuestions} /> : <QuestionList onDeleteQuestion={deleteQuestion} setQuestions={setQuestions} questions={questions} />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, onDeleteQuestion, setQuestions }) {
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map(question => (
+          <QuestionItem 
+            key={question.id}
+            question={question}
+            questions={questions}
+            onDeleteQuestion={onDeleteQuestion}
+            setQuestions={setQuestions}
+          />
+        ))}
+        </ul>
     </section>
   );
 }
